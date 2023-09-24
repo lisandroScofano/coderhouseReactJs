64a52221296ae308ccdb3e9ecb936a204023f848
Lisandro Scofano <lisandroscofano@gmail.com>
2023-09-24 13:26:07 -0300

    cambios en cart y checkout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,46 @@
+import { useContext } from "react"
+import { CartContext } from "../context/CartContext";
+
 export const Cart = () => {
+
+    const { cart, totalCartAmount } = useContext(CartContext);
+    if (cart.length == 0) {
+        return (
+            <div className="container text-center">
+                <h1>El carrito de compra esta vacio</h1>
+            </div>
+        )
+    }
+
     return (
-        <>
-            <h1>Este es el componente Cart</h1>
-        </>
+        <div className="container text-center w-50">
+            <h1>Su carrito de compra incluye</h1>
+            <div className="table-responsive">
+                <table className="table table-striped table-bordered table-hover mt-5">
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cart.map((prod) => (
+                            <tr key={prod.id}>
+                                <td>{prod.title}</td>
+                                <td>{prod.cantidad}</td>
+                                <td>{prod.price}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th colSpan={2}>Total</th>
+                            <td>{totalCartAmount()}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
     )
 }
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,13 +1,17 @@
 import { useContext, useState } from "react"
 import { getFirestore, collection, addDoc } from "firebase/firestore"
 import { CartContext } from "../context/CartContext";
+import { Cart } from "./Cart";
 
 export const Checkout = () => {
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const [email2, setEmail2] = useState("");
     const [phone, setPhone] = useState("");
     const [orderId, setOrderId] = useState();
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+
 
     const { cart, totalCartAmount } = useContext(CartContext);
 
@@ -27,30 +31,58 @@ export const Checkout = () => {
         const ordenesRef = collection(db, "ordenes");
         addDoc(ordenesRef, order).then(result => setOrderId(result.id));
     }
+    function verificarEmail() {
+        if (email === email2) {
+            setButtonDisabled(false)
+        } else {
+            setButtonDisabled(true)
+        }
+    }
 
     if (orderId) {
         return (
-            <h1>{`Gracias por tu compra ${orderId}!`} </h1>
+            <div className="container w-75 mt-5 text-center">
+                <h1>{`Gracias por su compra! El ID de la orden es: ${orderId}`} </h1>
+            </div>
         )
     }
 
-    return (
-        <div className="container w-25">
-            <form onSubmit={(e) => e.preventDefault()}>
-                <div className="mb-3">
-                    <label htmlFor="nombre" className="form-label">Nombre</label>
-                    <input type="text" className="form-control" id="nombre" value={name} onChange={(event) => setName(event.target.value)} />
+    if (cart.length == 0) {
+        return (
+            <Cart />
+        )
+    } else {
+        return (
+            <>
+                <Cart />
+                <div className="container w-50 mt-5 text-center">
+                    <h3>Complete los siguientes datos para confirmar su orden</h3>
+                    <div className="container w-50 mt-5">
+                        <form onSubmit={(e) => e.preventDefault()}>
+                            <div className="mb-3">
+                                <label htmlFor="nombre" className="form-label">Nombre</label>
+                                <input type="text" className="form-control" id="nombre" value={name} onChange={(event) => setName(event.target.value)} />
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="telefono" className="form-label">Telefono</label>
+                                <input type="tel" className="form-control" id="telefono" aria-describedby="emailHelp" value={phone} onChange={(event) => setPhone(event.target.value)} />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onBlur={verificarEmail} value={email} onChange={(event) => setEmail(event.target.value)} />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">Confirmar Email</label>
+                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onBlur={verificarEmail} value={email2} onChange={(event) => setEmail2(event.target.value)} />
+                            </div>
+                            <div>
+                                <button className="btn btn-primary" onClick={crearOrden} disabled={buttonDisabled}>Finalizar Compra</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={email} onChange={(event) => setEmail(event.target.value)} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="telefono" className="form-label">Telefono</label>
-                    <input type="tel" className="form-control" id="telefono" aria-describedby="emailHelp" value={phone} onChange={(event) => setPhone(event.target.value)} />
-                </div>
-                <button className="btn btn-primary" onClick={crearOrden}>Finalizar Compra</button>
-            </form>
-        </div>
-    )
+            </>
+        )
+    }
 }
